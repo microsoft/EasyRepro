98d41d437b53adebe5e3fecfeacfa0b1a9eb1164
darent <darent@DESKTOP-BK7ODUF>
2017-06-02 11:02:37 -0500

    Removed unused variables

--- TestSettings.cs
+++ TestSettings.cs
@@ -4,12 +4,6 @@ namespace Microsoft.Dynamics365.UIAutomation.UnitTests.Sample
 {
     public static class TestSettings
     {
-        public static string AccountLogicalName = "account";
-        public static string AccountId = "BD8AC246-2416-E711-8104-FC15B4282DF4";
-
-        public static string LookupField = "primarycontactid";
-        public static string LookupName = "Rene Valdes (sample)";
-
         public static BrowserOptions Options = new BrowserOptions
                                                 {
                                                     BrowserType = BrowserType.Chrome,
