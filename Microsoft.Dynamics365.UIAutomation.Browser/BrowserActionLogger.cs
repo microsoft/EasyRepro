df514fbd6d37e445da83cb46fa279472a3823a48
Administrator <Administrator@DARENT-SURFACE4>
2017-08-24 12:40:51 -0700

    Adding the API project

---
+++ Microsoft.Dynamics365.UIAutomation.Browser/BrowserActionLogger.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using System.Collections.Generic;
+
+namespace Microsoft.Dynamics365.UIAutomation.Browser
+{
+    public interface IBrowserActionLogger
+    {
+        void LogEvent(BrowserRecordingEvent @event);
+        void LogEvents(IList<BrowserRecordingEvent> events);
+    }
+
+    public abstract class BrowserActionLogger : IBrowserActionLogger
+    {
+        public abstract void LogEvent(BrowserRecordingEvent @event);
+        public abstract void LogEvents(IList<BrowserRecordingEvent> events);
+    }
+
+    public class InMemoryBrowserActionLogger : BrowserActionLogger
+    {
+        public InMemoryBrowserActionLogger()
+        {
+            this.Events = new List<BrowserRecordingEvent>();
+        }
+
+        public List<BrowserRecordingEvent> Events { get; private set; }
+
+        public override void LogEvent(BrowserRecordingEvent @event)
+        {
+            this.Events.Add(@event);
+        }
+
+        public override void LogEvents(IList<BrowserRecordingEvent> events)
+        {
+            this.Events.AddRange(events);
+        }
+    }
+}
---
+++ Microsoft.Dynamics365.UIAutomation.Browser/BrowserInitiationSource.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+namespace Microsoft.Dynamics365.UIAutomation.Browser
+{
+    public enum BrowserInitiationSource
+    {
+        NewBrowser,
+        Inline
+    }
+}
