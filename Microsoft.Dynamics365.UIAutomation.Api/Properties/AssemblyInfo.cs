63907b54b4049b1f42f76e1176e2141eddd30f28
v-utsrid <v-utsrid@WIN-KS12VUFTE2T>
2017-08-28 15:13:36 -0700

    Move API and browser project

---
+++ Microsoft.Dynamics365.UIAutomation.Api/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Microsoft.Dynamics365.UIAutomation.Api")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Microsoft.Dynamics365.UIAutomation.Api")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dfe2c141-f1aa-4d4b-8df8-0fb93023cf45")]
---
+++ Microsoft.Dynamics365.UIAutomation.Browser/BrowserType.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+namespace Microsoft.Dynamics365.UIAutomation.Browser
+{
+    public enum BrowserType
+    {
+        IE = 1,
+        Chrome = 2,
+        Firefox = 4,
+        Edge = 8,
+        PhantomJs = 16,
+        Remote = 32
+    }
+}
