d6934e0fb29e8ad7d03b65d7df00df886fc3349a
Tyler Olson (CRM) <tylerol@microsoft.com>
2018-06-20 10:58:15 -0700

    Updating Assemblies for v9 to 9.0.1.4 to match latest NuGet Version

--- Microsoft.Dynamics365.UIAutomation.Api/Properties/AssemblyInfo.cs
+++ Microsoft.Dynamics365.UIAutomation.Api/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Microsoft.Dynamics365.UIAutomation.Api")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,3 +21,6 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("dfe2c141-f1aa-4d4b-8df8-0fb93023cf45")]
+[assembly: AssemblyVersion("9.0.1.4")]
+[assembly: AssemblyFileVersion("9.0.1.4")]
+
