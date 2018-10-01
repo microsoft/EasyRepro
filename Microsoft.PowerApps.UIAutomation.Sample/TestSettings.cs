22ecc1d3be982440772a4c0bcd155828257d3096
ggeiszle <ggeiszle@GGEISZLEAPRIL10>
2018-10-01 13:15:13 -0500

    Changes to remove RunSettings Config and TIE domain

--- Microsoft.PowerApps.UIAutomation.Sample/TestSettings.cs
+++ Microsoft.PowerApps.UIAutomation.Sample/TestSettings.cs
@@ -1,26 +1,15 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 using Microsoft.Dynamics365.UIAutomation.Browser;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Configuration;
 
 namespace Microsoft.PowerApps.UIAutomation.Sample
 {
     public class TestSettings
     {
 
-        public TestContext TestContext { get; set; }
-
-        private static TestContext _testContext;
-
-        private static BrowserType Type;
-
-        [ClassInitialize]
-        public static void Initialize(TestContext TestContext)
-        {
-            _testContext = TestContext;
-            Type = (BrowserType)Enum.Parse(typeof(BrowserType), _testContext.Properties["BrowserType"].ToString());
-        }
+        private static string Type = ConfigurationManager.AppSettings["BrowserType"].ToString();
 
         public static BrowserOptions Options = new BrowserOptions
         {
