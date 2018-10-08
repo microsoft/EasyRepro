3b61a0be1566a6cdce919ca96de85ee671dd018d
Tyler Olson (CRM) <tylerol@microsoft.com>
2018-10-08 16:15:09 -0500

    Resolving Conflicts

--- Microsoft.PowerApps.UIAutomation.Sample/TestSettings.cs
+++ Microsoft.PowerApps.UIAutomation.Sample/TestSettings.cs
@@ -1,14 +1,14 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 using Microsoft.Dynamics365.UIAutomation.Browser;
-using System.Configuration;
 using System;
+using System.Configuration;
 
 namespace Microsoft.PowerApps.UIAutomation.Sample
 {
     public static class TestSettings
     {
-        private static readonly string Type = ConfigurationManager.AppSettings["BrowserType"].ToString();
+        private static string Type = ConfigurationManager.AppSettings["BrowserType"].ToString();
 
         public static BrowserOptions Options = new BrowserOptions
         {
