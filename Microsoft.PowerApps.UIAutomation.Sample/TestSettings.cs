a85b54694ab6ace5933d3ab05891c160ecf8baab
Tyler Olson <tylerol@microsoft.com>
2018-10-11 16:14:55 -0500

    Updating test to make use of runsettings file, plus parameters for solution name and environment name (#205)

--- Microsoft.PowerApps.UIAutomation.Sample/TestSettings.cs
+++ Microsoft.PowerApps.UIAutomation.Sample/TestSettings.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 using Microsoft.Dynamics365.UIAutomation.Browser;
-using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Microsoft.PowerApps.UIAutomation.Sample
 {
