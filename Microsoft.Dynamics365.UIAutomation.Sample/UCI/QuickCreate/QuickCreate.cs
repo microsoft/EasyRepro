e601dd035035185da068595f93a1c27dd6b3d20a
Daren Turner <darent@microsoft.com>
2018-11-26 16:21:42 -0600

    QuickCreate Functionality (#260)

---
+++ Microsoft.Dynamics365.UIAutomation.Sample/UCI/QuickCreate/QuickCreate.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved. 
+// Licensed under the MIT license. 
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Dynamics365.UIAutomation.Api.UCI;
+using Microsoft.Dynamics365.UIAutomation.Browser;
+using System;
+using System.Security;
+
+namespace Microsoft.Dynamics365.UIAutomation.Sample.UCI
+{
+    [TestClass]
+    public class QuickCreateContactUCI
+    {
+        private readonly SecureString _username = System.Configuration.ConfigurationManager.AppSettings["OnlineUsername"].ToSecureString();
+        private readonly SecureString _password = System.Configuration.ConfigurationManager.AppSettings["OnlinePassword"].ToSecureString();
+        private readonly Uri _xrmUri = new Uri(System.Configuration.ConfigurationManager.AppSettings["OnlineCrmUrl"].ToString());
+
+        [TestMethod]
+        public void UCITestQuickCreateContact()
+        {
+            var client = new WebClient(TestSettings.Options);
+            using (var xrmApp = new XrmApp(client))
+            {
+                xrmApp.OnlineLogin.Login(_xrmUri, _username, _password);
+
+                xrmApp.Navigation.OpenApp("UCI");
+                
+                xrmApp.Navigation.QuickCreate("contact");
+                
+                xrmApp.QuickCreate.SetValue("firstname", "UCI");
+                
+                xrmApp.QuickCreate.SetValue("lastname", "EasyRepro10");
+                
+                xrmApp.QuickCreate.Save();
+                
+            }
+        }
+    }
+}
