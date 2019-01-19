08083ca1c9542cc85a1f95d5a4332f28643e0692
ggeiszle <ggeiszle@GGEISZLEAPRIL10>
2019-01-18 18:19:27 -0600

    Updates to some Elements to document APIs

--- Microsoft.Dynamics365.UIAutomation.Api.UCI/Elements/Grid.cs
+++ Microsoft.Dynamics365.UIAutomation.Api.UCI/Elements/Grid.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 using Microsoft.Dynamics365.UIAutomation.Browser;
+using System.Collections.Generic;
 
 namespace Microsoft.Dynamics365.UIAutomation.Api.UCI
 {
@@ -13,32 +14,58 @@ namespace Microsoft.Dynamics365.UIAutomation.Api.UCI
             _client = client;
         }
 
+        /// <summary>
+        /// Switches the view to the view supplied
+        /// </summary>
+        /// <param name="viewName">Name of the view to select</param>
         public void SwitchView(string viewName)
         {
-            _client.SwitchView(viewName, _client.Browser.Options.DefaultThinkTime);
+            _client.SwitchView(viewName);
         }
+
+        /// <summary>
+        /// Opens a record in the grid
+        /// </summary>
+        /// <param name="index">The index of the row to open</param>
         public void OpenRecord(int index)
         {
-            _client.OpenRecord(index, _client.Browser.Options.DefaultThinkTime);
+            _client.OpenRecord(index);
         }
+
+        /// <summary>
+        /// Performs a Quick Find on the grid
+        /// </summary>
+        /// <param name="searchCriteria">Search term</param>
+        /// <param name="clearByDefault">Determines whether to clear the quick find field before supplying the search term</param>
         public void Search(string searchCriteria, bool clearByDefault = true)
         {
-            _client.Search(searchCriteria, clearByDefault, _client.Browser.Options.DefaultThinkTime);
+            _client.Search(searchCriteria, clearByDefault);
         }
 
+        /// <summary>
+        /// Clears the quick find field
+        /// </summary>
         public void ClearSearch()
         {
-            _client.ClearSearch(_client.Browser.Options.DefaultThinkTime);
+            _client.ClearSearch();
         }
 
-        public void GetGridItems()
+        /// <summary>
+        /// Returns a list of items from the current grid
+        /// </summary>
+        /// <returns></returns>
+        public List<GridItem> GetGridItems()
         {
-            _client.GetGridItems(_client.Browser.Options.DefaultThinkTime);
+            return _client.GetGridItems();
         }
 
+        /// <summary>
+        /// Sorts the grid by the column provided
+        /// </summary>
+        /// <param name="columnName">Label of the column name</param>
         public void Sort(string columnName)
         {
-            _client.Sort(columnName, _client.Browser.Options.DefaultThinkTime);
+            _client.Sort(columnName);
         }
     }
 }
