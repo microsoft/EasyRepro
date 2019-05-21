e7ddd709e566de2e545d7cd91a074cf1209c2bf5
Jason Lattimer <jason.lattimer@gmail.com>
2019-05-21 16:53:19 -0400

    GitHub issue #452 Added UCI  new method to use form selector to change forms (#486)

---
+++ Microsoft.Dynamics365.UIAutomation.Sample/UCI/Navigation/FormSelector.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Dynamics365.UIAutomation.Api.UCI;
+using Microsoft.Dynamics365.UIAutomation.Browser;
+using System;
+using System.Security;
+
+namespace Microsoft.Dynamics365.UIAutomation.Sample.UCI
+{
+    [TestClass]
+    public class FormSelectorUci
+    {
+        private readonly SecureString _username = System.Configuration.ConfigurationManager.AppSettings["OnlineUsername"].ToSecureString();
+        private readonly SecureString _password = System.Configuration.ConfigurationManager.AppSettings["OnlinePassword"].ToSecureString();
+        private readonly Uri _xrmUri = new Uri(System.Configuration.ConfigurationManager.AppSettings["OnlineCrmUrl"]);
+
+        [TestMethod]
+        public void UCITestOpenGlobalSearch()
+        {
+            var client = new WebClient(TestSettings.Options);
+            using (var xrmApp = new XrmApp(client))
+            {
+                xrmApp.OnlineLogin.Login(_xrmUri, _username, _password);
+                
+                xrmApp.Navigation.OpenApp(UCIAppName.Sales);
+
+                xrmApp.Navigation.OpenSubArea("Sales", "Accounts");
+
+                xrmApp.Grid.OpenRecord(0);
+
+                xrmApp.Entity.SelectForm("Account");
+
+                xrmApp.ThinkTime(3000);
+
+                xrmApp.Entity.SelectForm("AI for Sales");
+
+                xrmApp.ThinkTime(3000);
+            }
+        }
+    }
+}
