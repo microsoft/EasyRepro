f9ce4273b467be75af538f09d3572f5c06b691d9
Jason Lattimer <jason.lattimer@gmail.com>
2019-06-04 10:04:57 -0400

    Web subgrid support (#521)
    * Added subgrid get records, get record count, open, & delete (#462 #156)
    
    * Updated comments to specify unique name and not label is being used because we can't rely on the label always being displayed.
    
    * Moved subgrid functionality to entity

---
+++ Microsoft.Dynamics365.UIAutomation.Sample/Web/Entity/SubGrid.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Dynamics365.UIAutomation.Api;
+using Microsoft.Dynamics365.UIAutomation.Browser;
+using System;
+using System.Security;
+using System.Collections.Generic;
+
+namespace Microsoft.Dynamics365.UIAutomation.Sample.Web
+{
+    [TestClass]
+    public class SubGrid
+    {
+        private readonly SecureString _username = System.Configuration.ConfigurationManager.AppSettings["OnlineUsername"].ToSecureString();
+        private readonly SecureString _password = System.Configuration.ConfigurationManager.AppSettings["OnlinePassword"].ToSecureString();
+        private readonly Uri _xrmUri = new Uri(System.Configuration.ConfigurationManager.AppSettings["OnlineCrmUrl"].ToString());
+
+        [TestMethod]
+        public void WEBTestOpenSubGridRecord()
+        {
+            using (var xrmBrowser = new Api.Browser(TestSettings.Options))
+            {
+                xrmBrowser.LoginPage.Login(_xrmUri, _username, _password);
+
+                xrmBrowser.GuidedHelp.CloseGuidedHelp();
+
+                xrmBrowser.Navigation.OpenSubArea("Sales", "Accounts");
+
+                xrmBrowser.Grid.SwitchView("Active Accounts");
+
+                xrmBrowser.Grid.OpenRecord(0);
+
+                //This uses the unique name (not label) from the subgrid properties
+                xrmBrowser.Entity.OpenSubGridRecord("CONTACTS", 0);
+
+                xrmBrowser.ThinkTime(2000);
+            }
+        }
+
+        [TestMethod]
+        public void WEBTestDeleteSubGridRecord()
+        {
+            using (var xrmBrowser = new Api.Browser(TestSettings.Options))
+            {
+                xrmBrowser.LoginPage.Login(_xrmUri, _username, _password);
+
+                xrmBrowser.GuidedHelp.CloseGuidedHelp();
+
+                xrmBrowser.Navigation.OpenSubArea("Sales", "Accounts");
+
+                xrmBrowser.Grid.SwitchView("Active Accounts");
+
+                xrmBrowser.Grid.OpenRecord(0);
+
+                //This uses the unique name (not label) from the subgrid properties
+                xrmBrowser.Entity.DeleteSubGridRecord("CONTACTS", 0);
+
+                xrmBrowser.ThinkTime(2000);
+            }
+        }
+
+        [TestMethod]
+        public void WEBTestGetSubGridItems()
+        {
+            using (var xrmBrowser = new Api.Browser(TestSettings.Options))
+            {
+                xrmBrowser.LoginPage.Login(_xrmUri, _username, _password);
+
+                xrmBrowser.GuidedHelp.CloseGuidedHelp();
+
+                xrmBrowser.Navigation.OpenSubArea("Sales", "Accounts");
+
+                xrmBrowser.Grid.SwitchView("Active Accounts");
+
+                xrmBrowser.Grid.OpenRecord(0);
+
+                //This uses the unique name (not label) from the subgrid properties
+                List<GridItem> contacts = xrmBrowser.Entity.GetSubGridItems("CONTACTS");
+
+                //This uses the unique name (not label) from the subgrid properties
+                var contactCount = xrmBrowser.Entity.GetSubGridItemsCount("CONTACTS");
+
+                xrmBrowser.ThinkTime(2000);
+            }
+        }
+    }
+}
