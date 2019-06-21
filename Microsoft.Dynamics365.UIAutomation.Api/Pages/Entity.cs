7dff4355339b84cfbc0cbbd8cba9f4b6095f9de9
Thathi Pammi <thathi123@gmail.com>
2019-06-21 12:45:49 +0100

    Fixed GetHeaderValue

--- Microsoft.Dynamics365.UIAutomation.Api/Pages/Entity.cs
+++ Microsoft.Dynamics365.UIAutomation.Api/Pages/Entity.cs
@@ -970,9 +970,13 @@ namespace Microsoft.Dynamics365.UIAutomation.Api
                     {
                         text = fieldElement.FindElement(By.TagName("textarea")).GetAttribute("value");
                     }
-                    else
+                    else if (fieldElement.FindElements(By.TagName("input")).Count > 0)
                     {
                         text = fieldElement.FindElement(By.TagName("input")).GetAttribute("value");
+                    }
+                    else
+                    {
+                        text = fieldElement.Text;
                     }
                 }
                 else
