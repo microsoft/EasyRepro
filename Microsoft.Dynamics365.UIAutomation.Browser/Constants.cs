102e8569c5ab0f401b05cbd8e8467e92b172d053
jens-voigt <60212554+jens-voigt@users.noreply.github.com>
2021-07-05 12:50:32 +0200

    Update Constants to support german online environments
    The suffix for german online environments is ".crm16.dynamics.com", so in order to make the login work, it should be added to the constants.
    
    Also crm14 for ZAF and crm17 for CHE.
    
    Refer to the official documentation: https://docs.microsoft.com/en-us/power-platform/admin/new-datacenter-regions

--- Microsoft.Dynamics365.UIAutomation.Browser/Constants.cs
+++ Microsoft.Dynamics365.UIAutomation.Browser/Constants.cs
@@ -97,7 +97,7 @@ namespace Microsoft.Dynamics365.UIAutomation.Browser
                 ".crm.dynamics.com", ".crm2.dynamics.com", ".crm3.dynamics.com",
                 ".crm4.dynamics.com", "crm5.dynamics.com", "crm6.dynamics.com", "crm7.dynamics.com",
                 ".crm8.dynamics.com", ".crm9.dynamics.com", ".crm10.dynamics.com", ".crm11.dynamics.com",
-                ".crm12.dynamics.com", ".crm15.dynamics.com", "portal.office.com", "crm2.crmlivetie.com", ".crm.microsoftdynamics.us", "portal.office365.us"
+                ".crm12.dynamics.com", ".crm14.dynamics.com", ".crm15.dynamics.com", ".crm16.dynamics.com", ".crm17.dynamics.com", "portal.office.com", "crm2.crmlivetie.com", ".crm.microsoftdynamics.us", "portal.office365.us"
             };
         }
 
