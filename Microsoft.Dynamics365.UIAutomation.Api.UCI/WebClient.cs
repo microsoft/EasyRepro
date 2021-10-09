ed96fccae8c9cc8aa776572cc09fb374dda618d4
Max Ewing <max.ewing@outlook.com>
2021-10-09 16:52:41 +0100

    refactor: line endings

--- Microsoft.Dynamics365.UIAutomation.Api.UCI/WebClient.cs
+++ Microsoft.Dynamics365.UIAutomation.Api.UCI/WebClient.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
 using Microsoft.Dynamics365.UIAutomation.Api.UCI.DTO;
